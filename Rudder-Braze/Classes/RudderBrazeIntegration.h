0bc08543ee06e54bec05e1addcca102a89ea6d41
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2023-08-31 15:26:26 +0530

    feat: update braze sdk to latest version 6.6.0
    Push notification is yet to be removed/added

--- Rudder-Braze/Classes/RudderBrazeIntegration.h
+++ Rudder-Braze/Classes/RudderBrazeIntegration.h
@@ -6,15 +6,7 @@
 
 #import <Foundation/Foundation.h>
 
-#if defined(__has_include) && __has_include(<Appboy_iOS_SDK/AppboyKit.h>)
-#import <Appboy_iOS_SDK/AppboyKit.h>
-#import <Appboy_iOS_SDK/ABKUser.h>
-#import <Appboy_iOS_SDK/ABKAttributionData.h>
-#else
-#import "Appboy-iOS-SDK/AppboyKit.h"
-#import "Appboy-iOS-SDK/ABKUser.h"
-#import "Appboy-iOS-SDK/ABKAttributionData.h"
-#endif
+@import BrazeKit;
 #import <Rudder/Rudder.h>
  
 NS_ASSUME_NONNULL_BEGIN
@@ -37,6 +29,7 @@ typedef enum {
 
 @interface RudderBrazeIntegration : NSObject<RSIntegration> {
     ConnectionMode connectionMode;
+    Braze *braze;
 }
 
 @property (nonatomic, strong) NSDictionary *config;
